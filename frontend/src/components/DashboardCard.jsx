7c8a8fae35bac0c14f7edf120cde8eb06545cf8e
Pranav Gouraje <pranavgouraje@gmail.com>
2026-06-05 17:35:11 +0530

    Created Dashboard Card and displayed on Dashboard

---
+++ frontend/src/components/DashboardCard.jsx
@@ -0,0 +1,11 @@
+function DashboardCard({title,value}) {
+  return (
+    <div className="dashboard-card">
+      <h3>{title}</h3>
+
+      <h2>{value}</h2>
+    </div>
+  );
+}
+
+export default DashboardCard;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,34 @@
 import MainLayout from "../layouts/MainLayout";
+import DashboardCard from "../components/DashboardCard";
 
 function Dashboard() {
   return (
     <MainLayout>
       <h1>Dashboard</h1>
 
-      <p>Welcome to Dairy Management System</p>
+      <div className="dashboard-grid">
+
+        <DashboardCard
+          title="Total Members"
+          value="325"
+        />
+
+        <DashboardCard
+          title="Today's Milk"
+          value="1450 L"
+        />
+
+        <DashboardCard
+          title="Today's Amount"
+          value="₹45,620"
+        />
+
+        <DashboardCard
+          title="Pending Payments"
+          value="₹12,000"
+        />
+
+      </div>
     </MainLayout>
   );
 }
