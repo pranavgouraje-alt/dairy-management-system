6816139a5705527e29dd4788fefd4a5224b13027
Pranav Gouraje <pranavgouraje@gmail.com>
2026-07-01 22:02:05 +0530

    created basic Backend server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("Dairy ERP Backend Running");
+  res.send("Dairy Management Backend is running");
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
-}); 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
